5f9e9fa58d8abecca87e49b904a4736209b3f241
joaoespdev <joaokero@hotmail.com>
2025-01-09 11:44:56 -0300

    feat: solid-db

---
+++ Principles/src/main/java/com/equipeacademia/project/SrpRobot.java
@@ -0,0 +1,66 @@
+package com.equipeacademia.project;
+
+//S- Single Responsibility Principle
+// A classe deve ter apenas uma responsabilidade!
+public class SrpRobot {
+    public boolean turnon = true;
+
+
+    public void andar() {
+        System.out.println("==========================\nO robo está andando.");
+    }
+
+    SrpRobot () {
+        this.turnon = turnon;
+    }
+
+
+    public static class CheffRobot extends SrpRobot {
+        public void cozinhar() {
+            System.out.println("O robo está cozinhando. 🔥🔥");
+        }
+
+        public CheffRobot() {
+
+        }
+    }
+
+    public static class GardenerRobot extends SrpRobot {
+        public void plantar() {
+            System.out.println("O robo está plantando. 🌱");
+        }
+
+        public GardenerRobot() {
+
+        }
+    }
+
+    public static class DevRobot extends SrpRobot {
+        public void programar() {
+            System.out.println("O robo está programando. ☕🚀");
+        }
+
+        public DevRobot() {
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        CheffRobot cheffRobot1 = new CheffRobot();
+        cheffRobot1.andar();
+        cheffRobot1.cozinhar();
+
+        GardenerRobot gardenerRobot1 = new GardenerRobot();
+        gardenerRobot1.andar();
+        gardenerRobot1.plantar();
+
+        DevRobot devRobot1 = new DevRobot();
+        devRobot1.andar();
+        devRobot1.programar();
+
+    }
+
+}
---
+++ Principles/src/main/java/com/equipeacademia/project/WrongSrpRobot.java
@@ -0,0 +1,82 @@
+package com.equipeacademia.project;
+
+//S- Single Responsibility Principle
+// A classe deve ter apenas uma responsabilidade!
+public class WrongSrpRobot {
+    public boolean turnon = true;
+
+
+    public void andar() {
+        System.out.println("==========================\nO robo está andando.");
+    }
+
+    WrongSrpRobot () {
+        this.turnon = turnon;
+    }
+
+    public static class CheffRobot extends WrongSrpRobot {
+        public void cozinhar() {
+            System.out.println("O robo está cozinhando. 🔥🔥");
+        }
+
+        public CheffRobot() {
+
+        }
+    }
+
+    public static class GardenerRobot extends SrpRobot.CheffRobot {
+        public void plantar() {
+            System.out.println("O robo está plantando. 🌱");
+        }
+
+        public void colher() {
+            System.out.println("O robo está colhendo. 👩‍🌾");
+        }
+
+        public void vender() {
+            System.out.println("O robo está vendendo sua colheita. 🎩");
+        }
+
+        public void divulgar() {
+            System.out.println("O robo está divulgando sua mercadoria. 📢");
+        }
+
+        public GardenerRobot() {
+
+        }
+    }
+
+    public static class DevRobot extends SrpRobot.GardenerRobot {
+        public void programar() {
+            System.out.println("O robo está programando. ☕🚀");
+        }
+
+        public DevRobot() {
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+
+        CheffRobot cheffRobot1 = new CheffRobot();
+        cheffRobot1.andar();
+        cheffRobot1.cozinhar();
+
+        GardenerRobot gardenerRobot1 = new GardenerRobot();
+        gardenerRobot1.andar();
+        gardenerRobot1.cozinhar();
+        gardenerRobot1.plantar();
+        gardenerRobot1.colher();
+        gardenerRobot1.divulgar();
+        gardenerRobot1.vender();
+
+        DevRobot devRobot1 = new DevRobot();
+        devRobot1.andar();
+        devRobot1.programar();
+        devRobot1.plantar();
+
+    }
+
+}
